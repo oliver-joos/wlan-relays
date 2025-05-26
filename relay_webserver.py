1f3a00415216c94fb8873538e836a83543f616a3
Oliver Joos <oliver.joos@hispeed.ch>
2025-05-26 13:20:35 +0200

    Add service to GET /<file> with an example "www/index.html"
    Closes #1

--- relay_webserver.py
+++ relay_webserver.py
@@ -16,6 +16,7 @@ import sys
 import time
 import json
 import errno
+import os
 
 import uasyncio
 import network
@@ -32,22 +33,143 @@ except ImportError:
 HOSTNAME = "relay-server"
 LISTEN_IP = "0.0.0.0"
 PORT = 80
+STATIC_DIRPATH = "www/"
 
-HTTP_RESPONSE = """\
-HTTP/1.1 {status}
-Server: {server}
-Connection: close
+# MIME types of common filename extensions
+_MIMETYPE_OF_EXT = {
+    "html": "text/html",
+    "js": "application/javascript",
+    "xml": "text/xml",
+    "css": "text/css",
+    "png": "image/png",
+    "jpg": "image/jpg",
+    "gif": "image/gif",
+    "ico": "image/ico",
+    "json": "application/json",
+    "txt": "text/plain",
+    "log": "text/plain",
+    "csv": "text/csv",
+}
+_MIMETYPE_DEFAULT = "application/octet-stream"
 
-"""
+_CACHE_MAX_AGE = const(600)
+
+# Common HTTP status codes and reasons
+_STATUS_REASONS = {
+    200: "OK",
+    400: "Bad Request",
+    404: "Not Found",
+    500: "Internal Server Error",
+}
 
 # Map of Pin names to initialized Pin objects
 pin_cache = {}
 
 
+def guess_mimetype(pathname):
+    # Guess MIME type from pathname of a file
+    root, ext = pathname.rsplit(".", 1)
+    if ext.isdigit():
+        # Take name extension without ".<number>"
+        root, ext = root.rsplit(".", 1)
+    return _MIMETYPE_OF_EXT.get(ext, _MIMETYPE_DEFAULT)
+
+
+class Response:
+
+    def __init__(self, writer):
+        self.writer = writer
+        self.headers = {}
+
+    def set_header(self, key, value):
+        self.headers[key] = value
+
+    def start_response(self, status=200, content_len=0, content_type=None, gzipped=False):
+        print(f"Send HTTP status {status}")
+        write = self.writer.write
+        reason = _STATUS_REASONS.get(status)
+        if reason:
+            write(b"HTTP/1.1 %s %s\r\n" % (status, reason))
+        else:
+            write(b"HTTP/1.1 %s\r\n" % status)
+        if content_len is not None:
+            write(b"Content-Length: %s\r\n" % content_len)
+        if content_len:
+            if content_type:
+                if content_type.startswith("text/") and "charset=" not in content_type:
+                    write(b"Content-Type: %s;charset=utf-8\r\n" % content_type)
+                else:
+                    write(b"Content-Type: %s\r\n" % content_type)
+            if gzipped:
+                write(b"Content-Encoding: gzip\r\n")
+        for k_v in self.headers.items():
+            write(b"%s: %s\r\n" % k_v)
+        write(b"Server: %s\r\n\r\n" % HOSTNAME)
+
+    async def sendstream(self, f):
+        send_buffer = memoryview(bytearray(1024))
+        size = 0
+        while True:
+            nbytes = f.readinto(send_buffer)
+            if not nbytes:
+                break
+            if nbytes < len(send_buffer):
+                piece = send_buffer[:nbytes]
+            else:
+                piece = send_buffer
+            self.writer.write(piece)
+            size += nbytes
+        await self.writer.drain()
+        print(f"Sent {size} bytes")
+
+    async def sendfile(self, fpath, content_type=None, req_headers=None):
+        """Send a file as HTTP response to a request.
+
+        If we fail to read the file a response with HTTP status 404 will be sent.
+        If there is a request header "Last-Modified" with the mtime of the file then a
+        HTTP status 304 (Not Modified) with an empty body will be sent.
+
+        :param fpath:           absolute or relative path of the file
+        :param content_type:    content-type of the file, or None to guess it from file name
+        :param req_headers:     HTTP headers of the request as dict (e.g. to allow caching)
+        :raises OSError:        if an error occurs when opening or reading the file
+        """
+        error = None
+        for ending in (".gz", ""):
+            try:
+                fpath_ext = fpath + ending
+                fstat = os.stat(fpath_ext)
+                mtime = str(fstat[8])  # modification time
+                self.set_header("Last-Modified", mtime)
+                self.set_header("Cache-Control", f"max-age={_CACHE_MAX_AGE}")
+                if req_headers.get("if-modified-since") == mtime:
+                    # Send status "Not modified"
+                    self.start_response(status=304)
+                else:
+                    with open(fpath_ext, "rb") as f:
+                        self.start_response(
+                            content_len=fstat[6],
+                            content_type=content_type or guess_mimetype(fpath),
+                            gzipped=bool(ending)
+                        )
+                        print(f"Send {fpath_ext}")
+                        await self.sendstream(f)
+                error = None
+            except OSError as exc:
+                if exc.errno in (errno.ENOENT, errno.EINVAL, errno.EISDIR):
+                    error = exc
+                    continue
+                raise
+        if error:
+            raise error
+
+
 async def handle_request(reader, writer):
     """Handle an incoming HTTP request."""
+    method = None
     headers = {}
     status = None
+    resp = Response(writer)
 
     print("Receiving request:")
     while True:
@@ -61,10 +183,14 @@ async def handle_request(reader, writer):
             if line:
                 if status is None:
                     # First HTTP line
-                    if line.startswith("POST "):
+                    try:
+                        method, path = line.rsplit(" ", 3)[-3:-1]
+                    except ValueError:
+                        status = 400
+                    if method in ("GET", "POST"):
                         status = 200
                     else:
-                        status = 404
+                        status = 400
                 else:
                     # HTTP header line
                     key, value = line.split(":", 1)
@@ -88,7 +214,7 @@ async def handle_request(reader, writer):
 
     if status is not None:
         # Handle request
-        if body:
+        if method == "POST":
             props = json.loads(body)
             for pin_name, new_value in props.items():
                 try:
@@ -97,13 +223,19 @@ async def handle_request(reader, writer):
                     pin = pin_cache[pin_name] = Pin(pin_name, Pin.OUT, value=True)
                 print(f"Set {pin} to {'high' if new_value else 'low'}")
                 pin.value(bool(new_value))
-        else:
-            pass
+                resp.start_response(status=200)
 
-        print("Sending response:")
-        resp = HTTP_RESPONSE.format(status=status, server=HOSTNAME)
-        print('  ' + '\n  '.join(resp.splitlines()))
-        await writer.awrite(resp.replace('\n', '\r\n'))
+        elif method == "GET":
+            print(path)
+            if path.endswith("/"):
+                # Append default filename
+                path += "index.html"
+            path = STATIC_DIRPATH + path.lstrip("/")
+            try:
+                await resp.sendfile(path, req_headers=headers)
+            except OSError as exc:
+                print(f"Failed to access {path}: {exc}")
+                resp.start_response(status=404)
 
     await writer.aclose()
 
